7b75f5949ae87b0a706a7a6d3d1624ec930279f2
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-03-10 16:31:20 +0100

    Exercise4 completed

--- src/exercise3/Exercise3.java
+++ src/exercise3/Exercise3.java
@@ -25,7 +25,6 @@ public class Exercise3 {
         while (true) {
             System.out.println("Insert a string to divide it in single characters (type the string ':q' to exit):");
             String string = scanner.nextLine();
-            //StringBuilder stringDivided = new StringBuilder();
             if (Objects.equals(string, ":q")) break;
             for (int i = 0; i < string.length(); i++) {
                 if (i != string.length() - 1) {
--- src/exercise4/Exercise4.java
+++ src/exercise4/Exercise4.java
@@ -1,7 +1,39 @@
 package exercise4;
 
+import java.util.Scanner;
+
 public class Exercise4 {
+    static String countdownReport(int secondsToLaunch) {
+        StringBuilder countdown = new StringBuilder();
+        for (int i = secondsToLaunch; i > 0; i--) {
+
+            if (i % 2 == 0) {
+                countdown.append("[OK] ").append(i).append("\n");
+                if (i == 10) {
+                    countdown.append("--- SEPARATION STAGE ---").append("\n");
+                }
+            } else {
+                if (i != 1) {
+                    countdown.append("[CHECK] ").append(i).append("\n");
+                } else {
+                    countdown.append("[CHECK] ").append(i);
+                }
+            }
+
+        }
+        return countdown.toString();
+    }
+
+    ;
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert the amounts of seconds before launch: ");
+        int secondsToLaunch = Integer.parseInt(scanner.nextLine());
+        secondsToLaunch = secondsToLaunch < 0 ? -secondsToLaunch : secondsToLaunch;
+        secondsToLaunch = Math.min(secondsToLaunch, 20);
+        System.out.println("Seconds to launch: " + secondsToLaunch);
+        System.out.println(countdownReport(secondsToLaunch));
+        System.out.println("--- \uD83D\uDE80 IGNITION ---");
     }
 }
