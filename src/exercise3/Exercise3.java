b664622c3dcf42324e15bc53b733a8fa82f93187
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-03-10 15:36:34 +0100

    Exercise3 done

--- src/exercise3/Exercise3.java
+++ src/exercise3/Exercise3.java
@@ -1,7 +1,25 @@
 package exercise3;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Exercise3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Insert a string to divide it in single characters (type the string ':q' to exit):");
+            String stringToDivide = scanner.nextLine();
+            StringBuilder stringDivided = new StringBuilder();
+            if (Objects.equals(stringToDivide, ":q")) break;
+            for (int i = 0; i < stringToDivide.length(); i++) {
+                if (i != stringToDivide.length() - 1) {
+                    stringDivided.append(stringToDivide.charAt(i)).append(",");
+                } else {
+                    stringDivided.append(stringToDivide.charAt(i));
+                }
+            }
+            System.out.println(stringDivided);
+        }
     }
 }
