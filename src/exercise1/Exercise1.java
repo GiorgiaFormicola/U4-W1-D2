d20a8e64bd016e2dbda5a62a0c9d3d48acf2abf6
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-03-10 14:56:29 +0100

    Exercise1 completed

--- src/exercise1/Exercise1.java
+++ src/exercise1/Exercise1.java
@@ -1,7 +1,38 @@
 package exercise1;
 
 public class Exercise1 {
+    static boolean evenUnevenString(String string) {
+        return string.length() % 2 == 0;
+    }
+
+    static boolean leapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            return year % 4 == 0 && year % 100 != 0;
+        }
+    }
+
+    ;
+
     public static void main(String[] args) {
+        System.out.println("----- evenUnevenString METHOD TEST");
+        String string = "Ciao";
+        System.out.println("The string '" + string + "' as an " + (evenUnevenString(string) ? "even" : "uneven") + " number of characters");
+
+        string = "Ciao!";
+        System.out.println("The string '" + string + "' as an " + (evenUnevenString(string) ? "even" : "uneven") + " number of characters");
+
+        System.out.println("----- leapYear METHOD TEST");
+        int year = 2000;
+        System.out.println("The year " + year + " is" + (leapYear(year) ? "" : "not") + " a leap year");
+
+        year = 2004;
+        System.out.println("The year " + year + " is" + (leapYear(year) ? "" : "not") + " a leap year");
+
+        year = 1900;
+        System.out.println("The year " + year + " is" + (leapYear(year) ? "" : " not") + " a leap year");
+
 
     }
 }
