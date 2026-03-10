b1ac27ed5f47aa2e4dcc856f3445d20cc7d549f1
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-03-10 15:07:14 +0100

    Exercise2 completed

--- src/exercise2/Exercise2.java
+++ src/exercise2/Exercise2.java
@@ -1,7 +1,18 @@
 package exercise2;
 
+import java.util.Scanner;
+
 public class Exercise2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a number from 0 to 3 to print it:");
+        int number = Integer.parseInt(scanner.nextLine());
+        switch (number) {
+            case 0 -> System.out.println("Zero");
+            case 1 -> System.out.println("One");
+            case 3 -> System.out.println("Three");
+            default -> System.out.println("Error: the chosen number is not available");
+        }
+        ;
     }
 }
